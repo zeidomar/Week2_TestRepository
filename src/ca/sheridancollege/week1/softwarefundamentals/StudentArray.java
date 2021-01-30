1fa764812729cd3c125cf65ac6ec4a05ca5e278f
omarz <omarz@DESKTOP-FC212P3.lan>
2021-01-30 17:00:39 -0500

    This is my second commit.

--- src/ca/sheridancollege/week1/softwarefundamentals/StudentArray.java
+++ src/ca/sheridancollege/week1/softwarefundamentals/StudentArray.java
@@ -2,6 +2,7 @@
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
+ *this is my first commit.
  */
 package ca.sheridancollege.week1.softwarefundamentals;
 
