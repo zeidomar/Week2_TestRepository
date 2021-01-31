a229cad4720dd6eea8bdf097724986158d1bed64
omarz <omarz@DESKTOP-FC212P3.lan>
2021-01-30 19:38:09 -0500

    This is my third commit.

--- src/ca/sheridancollege/week1/softwarefundamentals/StudentArray.java
+++ src/ca/sheridancollege/week1/softwarefundamentals/StudentArray.java
@@ -3,6 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  *this is my first commit.
+ * testing getignore feature.
  */
 package ca.sheridancollege.week1.softwarefundamentals;
 
